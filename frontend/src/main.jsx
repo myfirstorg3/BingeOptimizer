df0d8d9dc45a232196ed0afcf6ad0fc1dbfa29d2
bandhuamajdoor <singhmanas2786@gmail.com>
2026-05-05 06:12:17 +0530

    nothing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,17 @@ import App from "./App.jsx";
 
 import { AuthProvider } from './context/AuthContext';
 
+const originalFetch = window.fetch;
+window.fetch = async (...args) => {
+  let [resource, config] = args;
+  config = config || {};
+  config.headers = {
+    ...config.headers,
+    'ngrok-skip-browser-warning': 'true'
+  };
+  return originalFetch(resource, config);
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
